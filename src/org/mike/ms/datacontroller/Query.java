2785be6247103c85cd6d28a9d9a226a745316182
Mike <qazwqsx9@gmail.com>
2019-11-13 08:16:43 -0500

    add to SQL

---
+++ src/org/mike/ms/datacontroller/Query.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package org.mike.ms.datacontroller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * @author c
+ *
+ */
+public class Query {
+	public ArrayList<String> path=new ArrayList<String>();
+	public HashMap<String, HashMap<String, String>> valueFile=new HashMap<String, HashMap<String,String>>();
+	public static String formate(Query query) {
+		String result="";
+		for(int i=0;i<query.path.size();i++) {
+			result+=query.path.get(i);
+			if(i!=query.path.size()-1)result+="@";
+		}
+		String valueString="";
+		ArrayList<String> fieldArrayList=new ArrayList<String>();
+		fieldArrayList.addAll(query.valueFile.keySet());
+		for(int i=0;i<fieldArrayList.size();i++) {
+			HashMap<String, String> value=query.valueFile.get(fieldArrayList.get(i));
+			valueString+=fieldArrayList.get(i);
+			
+			if(value.containsKey("VALUE")&&value.keySet().size()==1) {
+				valueString+=value.get("VALUE");
+			}else {
+				for(String key:value.keySet()) {
+					valueString+=key+"-"+value.get(key)+"/";
+				}
+				valueString.substring(0, valueString.length()-1);
+			}
+			if(i!=fieldArrayList.size()-1) {
+				valueString+=",";
+			}
+		}
+		return result+":"+valueString;
+	}
+	public static Query formate(String query) {
+		String[] pathValue=query.split(":");
+		String[] paths=pathValue[0].split("@");
+		Query q=new Query();
+		for(int i=0;i<paths.length;i++) {
+			q.path.add(paths[i]);
+		}
+		String[] values=pathValue[1].split(",");
+		for(int i=0;i<values.length;i++) {
+			HashMap<String, String> keySet=new HashMap<String, String>();
+			String[] fieldKeys=values[i].split("$");
+			if(!fieldKeys[1].contains("/")) {
+				keySet.put("VALUE", fieldKeys[1]);
+			}else {
+				String[] keyValueSet=fieldKeys[1].split("/");
+				for(String keyV:keyValueSet) {
+					String keyVStrings[]=keyV.split("-");
+					keySet.put(keyVStrings[0], keyVStrings[1]);
+				}
+			}
+			q.valueFile.put(fieldKeys[0],keySet);
+		}
+		return q;
+	}
+}
--- src/org/mike/ms/datacontroller/SQLInterface.java
+++ src/org/mike/ms/datacontroller/SQLInterface.java
@@ -5,10 +5,11 @@ package org.mike.ms.datacontroller;
 
 import java.io.Closeable;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * @author c
@@ -16,8 +17,7 @@ import java.util.HashMap;
  */
 // SQL@<>
 public class SQLInterface<T> implements DataInterface<T>,Closeable {
-    private Connection con = null ;
-    private Statement stmt = null ;
+    private Connection conn = null ;
     private String url = "jdbc:mysql://localhost/test ";
     private String user = "root ";
     private String pwd = "0429 ";
@@ -25,6 +25,8 @@ public class SQLInterface<T> implements DataInterface<T>,Closeable {
 	@Override
 	public T getData(Class source,String key) {
 		// TODO Auto-generated method stub
+		Query q=Query.formate(key);
+		if(!q.path.contains("SQL"))return null;
 		return null;
 	}
 
@@ -45,14 +47,106 @@ public class SQLInterface<T> implements DataInterface<T>,Closeable {
 		// TODO Auto-generated method stub
 		
 	}
-	private HashMap<String, String> search(){
-		
+	private ArrayList<HashMap<String, String>> search(String table,HashMap<String, String> keyValues){
+		if(table==null||table.equals("")||keyValues==null)return null;
+		String query="select * from "+table+" where ";
+		ArrayList<String> keys=new ArrayList<String>();
+		keys.addAll(keyValues.keySet());
+		for(int i=0;i<keys.size();i++) {
+			query+=keys.get(i)+"=?";
+			if(i!=keys.size()-1) {
+				query+=" and ";
+			}
+		}
+		try(PreparedStatement ps=conn.prepareStatement(query)){
+			for(int i=0;i<keys.size();i++) {
+				ps.setString(i+1,keyValues.get(keys.get(i)));
+			}
+	        ResultSet rs=ps.executeQuery();
+	        ResultSetMetaData rsmd = rs.getMetaData();
+			int length=rsmd.getColumnCount();
+			ArrayList<HashMap<String, String>> result=new ArrayList<HashMap<String,String>>();
+	        do {
+	        	HashMap<String, String> data=new HashMap<String, String>();
+	        	for(int i=0;i<length;i++) {
+	        		data.put(rsmd.getColumnLabel(i), rs.getString(i));
+	        	}
+	        	result.add(data);
+	        }while(rs.next());
+	        return result;
+		}catch (SQLException e ) {
+	        return null;
+	    }
+	}
+	private boolean update(String table,HashMap<String, String> keyValues,HashMap<String, String> updateValue) {
+		if(table==null||table.equals("")||keyValues==null||updateValue==null)return false;
+		ArrayList<HashMap<String, String>> result=search(table,keyValues);
+		ArrayList<String> keys=new ArrayList<String>();
+		ArrayList<String> updates=new ArrayList<String>();
+		keys.addAll(keyValues.keySet());
+		updates.addAll(updateValue.keySet());
+		String insert="insert into Goods (Gname,Gprice,Gamount,Gdate,Gperson) values (?,?,?,?,?)";
+
+		if(result==null) {
+			String updateString="";
+			String itemString="";
+			for(int i=0;i<updates.size();i++) {
+				itemString+=updateValue.get(updates.get(i));
+				updateString+="?";
+				if(i!=keys.size()-1) {
+					updateString+=",";
+					itemString+=",";
+				}
+			}
+			String query=String.format("insert into %s (%s) values (%s)", table,itemString,updateString);
+            try(PreparedStatement ps=conn.prepareStatement(query)) {
+            	for(int i=0;i<updates.size();i++) {
+    				ps.setString(i+1,updateValue.get(keys.get(i)));
+    			}
+				ps.executeUpdate();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return false;
+			}
+		}else {
+			String condic="";
+			String itemString="";
+			for(int i=0;i<keys.size();i++) {
+				condic+=keys.get(i)+"=?";
+				if(i!=keys.size()-1) {
+					condic+=" and ";
+				}
+			}
+			for(int i=0;i<updates.size();i++) {
+				itemString+=updateValue.get(updates.get(i))+"=?";
+				if(i!=keys.size()-1) {
+					itemString+=",";
+				}
+			}
+			String query=String.format("update %s set %s where %s", table,itemString,condic);
+            try(PreparedStatement ps=conn.prepareStatement(query)){
+            	int keySize=keys.size();
+    			int updateSize=updates.size();
+    			for(int i=0;i<keySize;i++) {
+    				ps.setString(i+1,keyValues.get(keys.get(i)));
+    			}
+                for(int i=0;i<updateSize;i++) {
+    				ps.setString(i+1+keySize,updateValue.get(updates.get(i)));
+    			}
+				ps.executeUpdate();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return false;
+			}
+		}
+		return true;
 	}
 	private void init(){
         try {        
-          Class.forName("com.mysql.jdbc.Driver ").newInstance();
-          con = DriverManager.getConnection(url ,user ,pwd );
-          stmt = con .createStatement();
+          Class.forName("com.mysql.jdbc.Driver").newInstance();
+          conn = DriverManager.getConnection(url ,user ,pwd );
         } catch (Exception e){
           // your installation of JDBC Driver Failed 
           e.printStackTrace();
@@ -62,8 +156,7 @@ public class SQLInterface<T> implements DataInterface<T>,Closeable {
 	public void close() throws IOException {
 		// TODO Auto-generated method stub
 		try {
-            if (con != null ) con .close();
-            if (stmt != null ) stmt .close();
+            if (conn != null ) conn .close();
         }catch (Exception e){
             e.printStackTrace();
         }
